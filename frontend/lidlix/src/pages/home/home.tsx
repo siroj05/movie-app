b1c213599062b024682ac92844d278cc8e584ae6
siroj05 <rojudin123@gmail.com>
2024-02-10 12:16:45 +0700

    card component

--- frontend/lidlix/src/pages/home/home.tsx
+++ frontend/lidlix/src/pages/home/home.tsx
@@ -1,6 +1,10 @@
 import MainNavbar from '../../components/navbar/navbar'
+import CardItem from '../../components/content/card-item'
 
 const Home = () => {
+  const cardItems = Array.from({ length: 12 }, (_, index) => (
+    <CardItem key={index}/>
+  ));
   return (
     <>
       <div>
@@ -8,8 +12,12 @@ const Home = () => {
       </div>
       {/* main konten */}
       <div className='flex justify-center mt-12'>
-        <div className='w-3/5 bg-black'>
-          test
+        <div className='w-3/5' data-theme='winter'>
+          <div className='p-8'>
+            <div className='grid grid-cols-4 gap-4'>
+              {cardItems}
+            </div>
+          </div>
         </div>
       </div>
     </>
