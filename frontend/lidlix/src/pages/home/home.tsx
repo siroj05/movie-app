a8e3c9d95235957816ebaa247d6911e91b4a47f7
DANDI CHRISTIAWAN <dandichristiawan@gmail.com>
2024-03-24 21:38:34 +0700

    benerin layout home sm routes kalo / langsung ke home

--- frontend/lidlix/src/pages/home/home.tsx
+++ frontend/lidlix/src/pages/home/home.tsx
@@ -1,27 +1,25 @@
-import MainNavbar from '../../components/navbar/navbar'
-import CardItem from '../../components/content/card-item'
+import MainNavbar from '../../components/navbar/navbar';
+import CardItem from '../../components/content/card-item';
 
 const Home = () => {
   const cardItems = Array.from({ length: 12 }, (_, index) => (
-    <CardItem key={index}/>
+    <CardItem key={index} />
   ));
   return (
     <>
       <div>
-        <MainNavbar/>
+        <MainNavbar />
       </div>
       {/* main konten */}
-      <div className='flex justify-center mt-12'>
-        <div className='w-3/5' data-theme='winter'>
-          <div className='p-8'>
-            <div className='grid grid-cols-4 gap-4'>
-              {cardItems}
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  )
-}
+      {/* <div className='flex justify-center mt-12'> */}
 
-export default Home
+      <div className="p-8">
+        <div className="grid grid-cols-4 gap-4">{cardItems}</div>
+      </div>
+
+      {/* </div> */}
+    </>
+  );
+};
+
+export default Home;
--- frontend/lidlix/src/routes/appRoutes.tsx
+++ frontend/lidlix/src/routes/appRoutes.tsx
@@ -10,7 +10,7 @@ const allRoutes = () => {
       element: <LoginPage />,
     },
     {
-      path: '/home',
+      path: '/',
       exact: true,
       element: <HomePage />,
     },
